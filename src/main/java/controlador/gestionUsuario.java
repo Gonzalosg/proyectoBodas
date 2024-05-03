df5d54b92e7f42417e320366ef6dc5cbfbaa3663
Gonza <168170589+Gonzalosg@users.noreply.github.com>
2024-05-03 17:07:21 +0200

    cambios peques

--- src/main/java/controlador/gestionUsuario.java
+++ src/main/java/controlador/gestionUsuario.java
@@ -64,6 +64,7 @@ public class gestionUsuario extends HttpServlet {
 		    usuario u = new usuario(nombre, apellido1, apellido2, email, fechaBoda,myHash, permiso);
 			
 			u.insertar();
+			response.sendRedirect("galeriaUsuarios.html");
 			System.out.println(u+" usuario insertado");
 		    
 		} catch (Exception e) {
